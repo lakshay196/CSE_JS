c62ef8619173c4e7f535ce4a3b4f831b08403510
Lakshay Gupta <lkshy196@gmail.com>
2026-02-24 10:16:19 +0530

    commit

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-console.log("Hello Wold");
-document.write("Hello World");
+
+var num = 10;
+console.log ( num );
+console.log ( typeof(num));
+num = false; 
+console.log( num);
+console.log ( typeof(num)); 
+num = undefined;
+console.log(num);
+console.log( typeof(num));
+num = null;
+console.log( num);
+console.log( typeof(num));
+num = 'Lakshay gupta';
+console.log( num);
+console.log( typeof(num));
